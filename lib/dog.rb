143e742d25219b11f4b95094b519a9a0564dcfc7
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-13 01:01:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,4 +8,13 @@ class Dog
   end 
   
   def self.create
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 end 
