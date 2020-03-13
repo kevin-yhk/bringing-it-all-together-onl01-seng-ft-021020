5db4030d58ca73abda81bfad5739f08055343ebe
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-13 01:38:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -31,7 +31,18 @@ class Dog
     new_dog 
   end
   
- 
+   def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
   
   
   # def self.find_by_name(name)
